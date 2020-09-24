f191e4f1de2666fd7fe50bbe26ccdd9a47d7fe33
An Nguyen <ancolincode@pop-os.localdomain>
2020-09-24 12:54:47 +0700

    Add effects to sidebar

--- main/script.js
+++ main/script.js
@@ -1,1 +1,19 @@
-var sidebarBtn = document.querySelector(".sidebar__button .btn");
+let sidebarBtn = document.querySelector(".sidebar__button")
+sidebarBtn.addEventListener("click", () => {
+  let dot = document.querySelector(".sidebar__button .dot")
+  let bar = document.querySelector(".sidebar__button .bar")
+  let sidebar = document.querySelector(".sidebar")
+
+  if (sidebarBtn.classList.contains("expand")) {
+    sidebar.classList.remove("expand")
+    sidebarBtn.classList.remove("expand")
+    dot.classList.remove("hidden")
+    bar.classList.add("hidden")
+  }
+  else {
+    sidebar.classList.add("expand")
+    sidebarBtn.classList.add("expand")
+    dot.classList.add("hidden")
+    bar.classList.remove("hidden")
+  }
+})
