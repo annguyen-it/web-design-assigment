ea07baff44beeb6e0e9e1b0dfda360d23e56b944
An Nguyen <ancolincode@pop-os.localdomain>
2020-09-24 22:39:35 +0700

    Optimize sidebar and add simple chart in page 'Tổng quan'

--- main/script.js
+++ main/script.js
@@ -17,3 +17,41 @@ sidebarBtn.addEventListener("click", () => {
     bar.classList.remove("hidden")
   }
 })
+
+
+
+let ctx = document.getElementById('myChart')
+let myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'],
+    datasets: [{
+      label: ['Bán vé'],
+      data: [12, 19, 3, 5, 2, 3, 2, 5, 16],
+      backgroundColor: 'rgba(255, 99, 132, 1)'
+    }, {
+      label: ['Đồ ăn/uống'],
+      data: [2, 9, 3, 5, 2, 13, 12, 5, 1],
+      backgroundColor: 'rgba(99, 99, 132, 1)'
+    }]
+  },
+  options: {
+    legend: {
+      display: true,
+      position: 'bottom',
+      fontSize: '15px'
+    },
+    responsive: true,
+    scales: {
+      xAxes: [{
+        stacked: true
+      }],
+      yAxes: [{
+        stacked: true,
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+})
