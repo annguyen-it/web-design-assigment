7c4d6a165ea8472c48fd194a33f31dc363f23ef8
An Nguyen <ancolincode@pop-os.localdomain>
2020-09-27 19:09:28 +0700

    Hot-fix bugs

--- main/ticket-management/script.js
+++ main/ticket-management/script.js
@@ -1,12 +1,20 @@
 let radios = document.form.tab
 let currTab = 0
 let dataArr = []
+let contents = []
 
 for (let i = 1; i <= 7; i++) {
-  fetch(`./content${ i }.html`)
-    .then(res => res.text())
-    .then(data => dataArr.push(data))
+  contents.push(`./content${ i }.html`)
 }
+
+let requests = contents.map(content => fetch(content))
+Promise.all(requests)
+  .then(responses => responses.forEach(
+    response => {
+      response.text()
+        .then(data => dataArr.push(data))
+    }
+  ))
 
 for (let i = 0; i < radios.length; i++) {
   radios[i].addEventListener('click', function () {
