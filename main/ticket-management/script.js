b7157a4582d384e335b5f0ddcbe9c8740d7b92e6
An Nguyen <ancolincode@pop-os.localdomain>
2020-09-30 13:47:14 +0700

    Upgrade ticket-management page

--- main/ticket-management/script.js
+++ main/ticket-management/script.js
@@ -1,28 +1,53 @@
-let radios = document.form.tab
-let currTab = 0
-let dataArr = []
+// Get data
 let contents = []
+let data = ''
 
 for (let i = 1; i <= 7; i++) {
   contents.push(`./content${ i }.html`)
 }
-let requests = contents.map(content => fetch(content))
 
-Promise.all(requests)
-  .then(responses => responses.forEach(
-    response => {
-      response.text()
-        .then(data => dataArr.push(data))
-    }
-  ))
+function getData() {
+  let element = document.getElementsByClassName('content')[0]
 
-for (let i = 0; i < radios.length; i++) {
-  radios[i].addEventListener('click', function () {
-    if (this.checked) {
-      if (i != currTab) {
-        currTab = i
-        document.querySelector('.content').innerHTML = dataArr[i]
+  for (let i = 0; i < contents.length; i++) {
+    const file = contents[i]
+    const parent = document.createElement('div')
+
+    if (file) {
+      const xhttp = new XMLHttpRequest()
+      xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          if (this.status === 200) {
+            parent.innerHTML = this.responseText
+          }
+
+          element.removeAttribute('w3-school')
+        }
       }
+
+      xhttp.open('GET', file)
+      xhttp.send()
+
+      element.append(parent)
+    }
+  }
+}
+
+getData()
+
+// Default content
+let tabs = document.querySelectorAll('.list > * > label')
+let pages = document.querySelectorAll('.content > *')
+let currentPage = 0
+pages[0].classList.add('display')
+
+// Show data
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    if (currentPage != index) {
+      pages[currentPage].classList.remove('display')
+      pages[index].classList.add('display')
+      currentPage = index
     }
   })
-}
+})
