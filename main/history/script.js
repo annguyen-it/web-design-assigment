10ce347cd2f7e52f5fe12cec89daf59a0cf2da2a
WRBKOR23 <haijdk2433@gmail.com>
2020-10-03 22:30:37 +0700

    update history, change history href in overview # -> link

---
+++ main/history/script.js
@@ -0,0 +1,186 @@
+var Include = {
+    includeHTML: function(page) {
+        var i, elem, contents = [], file, xhttp, // Khởi tạo các biến
+    
+        elem = document.getElementsByClassName('table')[0];
+        contents.push(`./content${ page }.html`)
+        // Lấy tất cả các thẻ có class là include-html (Như ở trên mình định nghĩa 2 thẻ để include header/footer)
+        // var includeFiles = document.getElementsByClassName('include-html');
+        
+        // Duyệt vòng lặp từ mảng phần tử ở trên
+        for (i = 0; i < 1; i++) {
+            file = contents[i];
+            // contents = includeFiles[i]; // Gán biến contents bằng thẻ đang được duyệt
+            // file = contents.getAttribute("data-file"); // Gán biến file là attribute của thẻ đang được duyệt
+    
+            if (file) { // Kiểm tra nếu tồn tại attribute này thì thực hiện tiếp
+                xhttp = new XMLHttpRequest(); // Khởi tạo request
+                xhttp.onreadystatechange = function() {
+                    // Lắng nghe event khi request kết thúc
+                    if (this.readyState == 4) {
+                        // Nếu status là 200 (thành công) thì set nội dung của file vào thẻ đang được duyệt
+                        if (this.status == 200) {elem.innerHTML = this.responseText;}
+    
+                        // Nếu status 404 (lỗi) thì hiển thị nội dung lỗi không thấy component
+                        if (this.status == 404) {elem.innerHTML = "Component not found.";}
+    
+                        // Sau khi duyệt xong thẻ này thì xoá class include-html 
+                        // elem.classList.remove("table");
+    
+                        // Gọi lại function includeHTML để duyệt tất cả các thẻ có class include-html còn lại
+                    }
+                }
+            }
+                // Tạo và gửi request
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return; // Thoát function
+        }
+    }
+};
+
+Include.includeHTML(1); 
+
+// pagination
+
+var Pagination = {
+
+    code: '',
+
+    // converting initialize data
+    Extend: function(data) {
+        data = data || {};
+        Pagination.size = data.size || 300;
+        Pagination.page = data.page || 1;
+        Pagination.step = data.step || 3;
+    },
+
+    // add pages by number (from [s] to [f])
+    Add: function(s, f) {
+        for (var i = s; i < f; i++) {
+            Pagination.code += '<a>' + i + '</a>';
+        }
+    },
+
+    // add last page with separator
+    Last: function() {
+        Pagination.code += '<i>...</i><a>' + Pagination.size + '</a>';
+    },
+
+    // add first page with separator
+    First: function() {
+        Pagination.code += '<a>1</a><i>...</i>';
+    },
+    // --------------------
+    // Handlers
+    // --------------------
+
+    // change page
+    Click: function() {
+        Pagination.page = +this.innerHTML;
+        Pagination.Start();
+        Include.includeHTML(Pagination.page);
+    },
+
+    // previous page
+    Prev: function() {
+        Pagination.page--;
+        if (Pagination.page < 1) {
+            Pagination.page = 1;
+        }
+        Pagination.Start();
+        Include.includeHTML(Pagination.page);
+    },
+
+    // next page
+    Next: function() {
+        Pagination.page++;
+        if (Pagination.page > Pagination.size) {
+            Pagination.page = Pagination.size;
+        }
+        Pagination.Start();
+        Include.includeHTML(Pagination.page);
+    },
+    // --------------------
+    // Script
+    // --------------------
+
+    // binding pages
+    Bind: function() {
+        var a = Pagination.e.getElementsByTagName('a');
+        for (var i = 0; i < a.length; i++) {
+            if (+a[i].innerHTML === Pagination.page) a[i].className = 'current';
+            a[i].addEventListener('click', Pagination.Click, false);
+        }
+    },
+
+    // write pagination
+    Finish: function() {
+        Pagination.e.innerHTML = Pagination.code;
+        Pagination.code = '';
+        Pagination.Bind();
+    },
+
+    // find pagination type
+    Start: function() {
+        if (Pagination.size < Pagination.step * 2 + 6) {
+            Pagination.Add(1, Pagination.size + 1);
+        }
+        else if (Pagination.page < Pagination.step * 2 + 1) {
+            Pagination.Add(1, Pagination.step * 2 + 4);
+            Pagination.Last();
+        }
+        else if (Pagination.page > Pagination.size - Pagination.step * 2) {
+            Pagination.First();
+            Pagination.Add(Pagination.size - Pagination.step * 2 - 2, Pagination.size + 1);
+        }
+        else {
+            Pagination.First();
+            Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
+            Pagination.Last();
+        }
+        Pagination.Finish();
+    },
+
+    // --------------------
+    // Initialization
+    // --------------------
+
+    // binding buttons
+    Buttons: function(e) {
+        var nav = e.getElementsByTagName('a');
+        nav[0].addEventListener('click', Pagination.Prev, false);
+        nav[1].addEventListener('click', Pagination.Next, false);
+    },
+
+    // create skeleton
+    Create: function(e) {
+
+        var html = [
+            '<a>&#9668;</a>', // previous button
+            '<span class="span-pagination" id="idspan-pagintaion"></span>',  // pagination container
+            '<a>&#9658;</a>'  // next button
+        ];
+
+        e.innerHTML = html.join('');
+        Pagination.e = e.getElementsByTagName('span')[0];
+        Pagination.Buttons(e);
+    },
+
+    // init
+    Init: function(e, data) {
+        Pagination.Extend(data);
+        Pagination.Create(e);
+        Pagination.Start();
+    }
+};
+
+var init = function() {
+    Pagination.Init(document.getElementById('pagination'), {
+        size: 15, // pages size
+        page: 1,  // selected page
+        step: 3   // pages before and after current
+    });
+};
+
+document.addEventListener('DOMContentLoaded', init, false);
